311eeb9bdcb411ffba90db562fb861364774d661
Joaquin Sanchez <tec.jesa1989@gmail.com>
2022-01-04 10:07:22 -0600

    OK
    JJJJ

--- WinFormsApp1/Form1.Designer.vb
+++ WinFormsApp1/Form1.Designer.vb
@@ -23,23 +23,35 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Label1.Location = New System.Drawing.Point(280, 147)
+        Me.Label1.Location = New System.Drawing.Point(311, 9)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(228, 37)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "PRUEBA PENDEJA"
         '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(37, 81)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(502, 37)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "FRANKLIN ES TONTO FRANKLIN ES NIÑA"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(800, 217)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -49,4 +61,5 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
 End Class
